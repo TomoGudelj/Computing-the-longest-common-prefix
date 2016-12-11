0fabc8a68bb43598719b0dc280a75dea1e130f04
TomoGudelj <tomislav.gudelj@fer.hr>
2016-12-11 17:19:55 +0100

    added and tested libdivsufsort library

--- ComputingTheLongestCommonPrefixArray/ComputingTheLongestCommonPrefixArray/MainFile.cpp
+++ ComputingTheLongestCommonPrefixArray/ComputingTheLongestCommonPrefixArray/MainFile.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "WaveletTree.h"
+#include "divsufsort.h"
 
 using namespace std;
 
@@ -9,14 +10,32 @@ int main(int argc, char *argv[]) {
 	/*Node n;
 	
 	n.BuildNode("arrd$rcbbraaaaaabba", {'$', 'a', 'b', 'c', 'd', 'r' }, 0, 5);*/
-	string S = "arrd$rcbbraaaaaabba";
+	//string S = "arrd$rcbbraaaaaabba";
+	//cin >> S;
+
+	string S;
 	cin >> S;
+	const char *str = S.c_str();
+
+	int S_length = S.size();
+
+	int *SA = (int *)malloc(S_length * sizeof(int));
+
+	divsufsort((unsigned char *)str, SA, S_length, 1);
+
+	for (int i = 0; i < S_length; ++i) {
+		printf("SA[%2d] = %2d: ", i, SA[i]);
+		for (int j = SA[i]; j < S_length; ++j) {
+			printf("%c", str[j]);
+		}
+		printf("$\n");
+	}
 
 	WaveletTree tree("arrd$rcbbraaaaaabba");
 
 	tree.BuildTree();
 
-	int rank = tree.CalculateRank('a', 18);
+	int rank = tree.CalculateRank('a', 0);
 
 	return 0;
 }
