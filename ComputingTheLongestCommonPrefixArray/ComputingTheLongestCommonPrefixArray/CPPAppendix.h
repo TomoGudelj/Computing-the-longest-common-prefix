278edb6724999848445fd84a6a22adc1479fb42b
Silvestar <bsilvestar@yahoo.com>
2017-01-05 22:28:38 +0100

    preuređen kod

--- ComputingTheLongestCommonPrefixArray/ComputingTheLongestCommonPrefixArray/CPPAppendix.h
+++ ComputingTheLongestCommonPrefixArray/ComputingTheLongestCommonPrefixArray/CPPAppendix.h
@@ -9,5 +9,16 @@ template<typename T>int IndexOf(std::vector<T> &vec, T el)
 	return find(vec.begin(), vec.end(), el) - vec.begin();
 }
 
+struct pair_hash {
+	template <class T1, class T2>
+	std::size_t operator () (const std::pair<T1, T2> &p) const {
+		auto h1 = std::hash<T1>{}(p.first);
+		auto h2 = std::hash<T2>{}(p.second);
+
+		// Mainly for demonstration purposes, i.e. works but is overly simple
+		// In the real world, use sth. like boost.hash_combine
+		return h1 ^ h2;
+	}
+};
 
 #endif // !_CPPAPPENDIX_H_
